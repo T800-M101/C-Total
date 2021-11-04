75b98d55a29980542ebec8bb56304c25c3404145
Guillermo Moran Lihaut <guillermo.moran@office365.3pillarglobal.com>
2021-11-04 12:51:36 -0500

    Metodos

---
+++ Metodos/Metodos/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace Metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SaludarAmigo("Juanito");
+
+
+            //Console.WriteLine( SumarValores());
+            //Console.WriteLine( Dividir());
+
+            //Operadores unarios => cambiad la expresión al valor contrario  y son - para numeros y ! para booleanos
+            //int num = 5;
+            //Console.WriteLine(-num);
+            //bool haceCalor = true;
+            //Console.WriteLine(!haceCalor);
+
+
+            // Operadores de adicion ++
+
+            //int num2 = 0;
+
+            //Console.WriteLine(num2); 
+            //Console.WriteLine(num2++);
+            //Console.WriteLine(num2);
+            //Console.WriteLine(num2++);
+            //Console.WriteLine(num2);
+
+            //Operadores de sustraccion --
+
+            //Operadores matemáticos + - * /
+
+            //Operador modular % (lo que sobra al dividir)
+
+            //Operadores relacionales < <> > 
+
+            //Operadores de igualdad == !=
+
+            //Operadores condicionales && ||
+
+
+        }
+
+
+        //public static void SaludarAmigo(string nombre)
+        //{
+        //    Console.WriteLine($"Hola {nombre}, que tengas un buen dia!");
+        //}
+
+        //public static int SumarValores()
+        //{
+
+        //    int val1 = 0;
+        //    int val2 = 0;
+        //    int resultado = 0;
+        //    Console.WriteLine("Escribe un valor");
+        //    try
+        //    {
+        //        val1 = Int32.Parse(Console.ReadLine());
+
+        //    }
+        //    catch (FormatException)
+        //    {
+
+        //        Console.WriteLine("El formato de entrada es incorrecto.");
+        //    }
+        //    catch (OverflowException)
+        //    {
+        //        Console.WriteLine("El numero ingresado es demasiado largo o demasiado corto.");
+        //    }
+        //    catch (ArgumentNullException)
+        //    {
+        //        Console.WriteLine("No se ha ingresado ningun valor.");
+        //    }
+
+
+        //    Console.WriteLine("Escribe otro valor");
+
+        //    try
+        //    {
+        //        val2 = Int32.Parse(Console.ReadLine());
+
+        //    }
+        //    catch (FormatException)
+        //    {
+
+        //        Console.WriteLine("El formato de entrada es incorrecto.");
+
+        //    }
+        //    catch (OverflowException)
+        //    {
+        //        Console.WriteLine("El numero ingresado es demasiado largo o demasiado corto.");
+        //    }
+        //    catch (ArgumentNullException)
+        //    {
+        //        Console.WriteLine("No se ha ingresado ningun valor.");
+        //    }
+        //    finally
+        //    {
+
+        //        resultado = val1 + val2;
+        //    }
+
+
+        //    return resultado;
+        //}
+
+
+        //public static int Dividir()
+        //{
+        //    int val1 = 0;
+        //    int val2 = 0;
+        //    int resultado = 0;
+        //    Console.WriteLine("Escribe un valor");
+        //    val1 = Int32.Parse(Console.ReadLine());
+
+        //    Console.WriteLine("Escribe un valor");
+        //    val2 = Int32.Parse(Console.ReadLine());
+
+        //    try
+        //    {
+        //        resultado = val1 / val2;
+        //    }
+        //    catch (DivideByZeroException)
+        //    {
+
+        //        Console.WriteLine("No se puede dividir entre cero.");
+        //    }
+
+        //    return resultado;
+
+        //}
+    }
+}
