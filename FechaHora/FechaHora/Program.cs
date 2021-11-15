c22d51a779f132098e13f3f4619f250a1728b56e
Guillermo Morán <tuahil@gmail.com>
2021-11-15 14:34:56 -0600

    DateTime & Nulabilidad

---
+++ FechaHora/FechaHora/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Globalization;
+
+namespace FechaHora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dar formato a una fecha
+            DateTime tiempo = new DateTime(1971,12,25);
+            Console.WriteLine($"Mi fecha de naciminetos es {tiempo}");
+
+            // Mostrar fecha actual
+            Console.WriteLine(DateTime.Today);
+
+            // Mostrar hora actual
+            Console.WriteLine(DateTime.Now);
+
+            // Saber el dia de mañana
+
+            DateTime tomorrow = ProximoDia();
+            Console.WriteLine($"Mañana será {tomorrow}");
+
+            // Obtener el dia de hoy
+            DateTime fechaActual = DateTime.Now;
+            string hoy = fechaActual.ToString("dddd", CultureInfo.CreateSpecificCulture("es-MX"));
+            string mes = fechaActual.ToString("MMMM", CultureInfo.CreateSpecificCulture("es-MX"));
+           
+            // string hoy = DateTime.Today.DayOfWeek;
+            Console.WriteLine($"Hoy es {hoy} de {mes}");
+
+            // Primer dia de un año
+            Console.WriteLine(PrimerDiaDelAnio(1999));
+
+            // Cuantos dias tiene un mes en particular
+            int dias = DateTime.DaysInMonth(2021,2);
+            Console.WriteLine($"El mes de Febrero de 2021 tuvo {dias} dias");
+
+            // Devolver una parte de la fecha
+            DateTime ahora = DateTime.Now;
+            Console.WriteLine($"Minuto: {ahora.Minute}");
+
+            Console.WriteLine($"Ahora son las {ahora.Hour} horas con {ahora.Minute} minutos y {ahora.Second} segundos");
+
+            // Dias de diferencia
+            Console.WriteLine("Por favor ingresa una fecha con el siguiente formato: aaaa-mm-dd");
+            string ingreso = Console.ReadLine();
+
+            if(DateTime.TryParse(ingreso, out tiempo))
+            {
+                Console.WriteLine(tiempo);
+                TimeSpan diasEnteros = ahora.Subtract(tiempo);
+                Console.WriteLine($"Dias que pasaron desde esa fecha: {diasEnteros.Days}");
+            }
+            else
+            {
+                Console.WriteLine("Ingreso incorrecto.");
+            }
+
+
+
+
+        }
+
+        static DateTime ProximoDia()
+        {
+            return DateTime.Today.AddDays(1);
+        }
+
+        static DateTime PrimerDiaDelAnio(int anio)
+        {
+            return new DateTime(anio, 1, 1);
+        }
+    }
+}
---
+++ Nulabilidad/Nulabilidad/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Nulabilidad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? num1 = null;
+            int? num2 = 1337;
+
+            double? num3 = new Double?();
+            double? num4 = 3.1416;
+
+            bool? valor = new bool?();
+            Console.WriteLine($"Nuestros valores son: {num1}, {num2}, {num3}, {num4}, {valor}");
+
+            bool? esHombre = null;
+
+            if(esHombre == true)
+            {
+                Console.WriteLine("El usuario es hombre");
+            }
+            else if(esHombre == false)
+            {
+                Console.WriteLine("El usuario es mujer");
+            }
+            else
+            {
+                Console.WriteLine("No se especificó genero");
+            }
+
+            double? num6 = 13.1;
+            double? num7 = null;
+            double num8;
+
+            if (num6 == null)
+            {
+                num8 = 0.00;
+            }
+            else
+            {
+                num8 = (double)num6;
+            }
+
+            Console.WriteLine($"El valor de num8 es {num8}");
+
+            num8 = num6 ?? 8.53; //13.1
+            Console.WriteLine($"El valor de num8 es {num8}");
+            num8 = num7 ?? 8.53; //8.53
+            Console.WriteLine($"El valor de num8 es {num8}");
+
+        }
+    }
+}
