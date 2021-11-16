433287ae7f4d767866fb7b9bfc94e4e38ef5447b
Guillermo Morán <tuahil@gmail.com>
2021-11-16 14:59:42 -0600

    Collections, Lists and LinkedList

---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        // Las colecciones permiten agregar elementos en tiempo de ejecución. Cosa que los arrays no.
+        static void Main(string[] args)
+        {
+            List<int> numeros = new List<int>();
+
+            //Console.WriteLine("¿Cuantos elementos quieres introducir?");
+            //int elementos = Int32.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < elementos; i++)
+            //{
+            //    Console.WriteLine($"Agrega el elemento {i + 1}");
+            //    int elemento = Int32.Parse(Console.ReadLine());
+            //    numeros.Add(elemento);
+
+            //}
+
+            //Console.WriteLine("Imprimiendo la lista de números");
+            //numeros.ForEach(numero => Console.WriteLine(numero));
+
+            Console.WriteLine("Introduce los elementos a la colección (0 para terminar)");
+            int elemento = 1;
+            
+            while(elemento != 0)
+            {
+                elemento = Int32.Parse(Console.ReadLine());
+                if(elemento != 0)
+                {
+                 numeros.Add(elemento);
+                }
+            }
+
+            //numeros.RemoveAt(numeros.Count -1);
+
+            Console.WriteLine("Imprimiendo elementos");
+            numeros.ForEach(numero => Console.WriteLine(numero));
+
+        }
+    }
+}
---
+++ CollectionsLinkedList/CollectionsLinkedList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> numeros = new LinkedList<int>();
+
+            foreach (int numero in new int[] { 10,9,8,7,6,5,4,3,2,1,0})
+            {
+                
+                numeros.AddFirst(numero);
+
+                
+            }
+
+            //numeros.Remove(6);
+            LinkedListNode<int> nodoImportante = new LinkedListNode<int>(15);
+           
+            numeros.AddFirst(nodoImportante);
+
+            //foreach (int numero in numeros)
+            //{
+            //    Console.WriteLine(numero);
+            //}
+
+            for (LinkedListNode<int> nodo = numeros.First; nodo != null; nodo = nodo.Next)
+            {
+                int numero = nodo.Value;
+                Console.WriteLine(numero);
+            }
+        }
+    }
+}
