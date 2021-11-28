f7004b353776487005443826dfa3c2dd02284700
Guillermo Morán <tuahil@gmail.com>
2021-11-27 22:37:14 -0600

    TaTeTi

---
+++ Arrays-TaTeTi/Arrays-TaTeTi/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Arrays_TaTeTi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaración de un array de 2 dimensiones
+            //string[,] matriz2D = new string[3, 2]
+            //{
+            //    { "Maria","López"},
+            //    { "Franco","Miller"},
+            //    { "Juana","Garay"},
+            //};
+
+            //Console.WriteLine("Ingresa el dato que quieres insertar");
+            //string dato = Console.ReadLine();
+            //Console.WriteLine("Ingresa el numero de fila donde esta el dato a remplazar:");
+            //int fila = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("Ingresa el numero de columna donde esta el dato a remplazar:");
+            //int columna = Int32.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Matriz antes del cambio:");
+            //Console.WriteLine("==================================");
+            //for(int i = 0; i < 3; i++)
+            //{
+            //    for(int j = 0; j < 2; j++)
+            //    {
+            //        Console.Write(matriz2D[i,j]);
+            //        Console.Write(" ");
+
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //matriz2D[fila,columna] = dato;
+
+            //Console.WriteLine("==================================");
+
+            //Console.WriteLine("Matriz despues del cambio:");
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 2; j++)
+            //    {
+            //        Console.Write(matriz2D[i, j]);
+            //        Console.Write(" ");
+
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //Console.WriteLine("==================================");
+
+
+
+            // Declaración de un array de 3 dimensiones
+            //string[,,] matriz3D = new string[,,]
+            //{
+            //    {
+            //    { "A","B","C",},
+            //    { "D","E","F",},
+            //    { "G","H","I",}
+            //    },
+            //    {
+            //    { "J","K","L",},
+            //    { "M","N","Ñ",},
+            //    { "O","P","Q",}
+            //    },
+            //    {
+            //    { "R","S","T",},
+            //    { "U","V","W",},
+            //    { "X","Y","Z",}
+            //    }
+            //};
+
+            //Console.WriteLine("El valor de A es: {0}", matriz3D[0,0,0]);
+            //Console.WriteLine("El valor de Q es: {0}", matriz3D[1,2,2]);
+            //Console.WriteLine("El valor de V es: {0}", matriz3D[2,1,1]);
+
+            
+
+
+        }
+
+    }
+}
